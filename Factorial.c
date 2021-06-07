d9467f9757a916246b554944e58e5454e3a7a5ec
Abigail G <79664752+Abynana@users.noreply.github.com>
2021-06-07 11:56:42 -0500

    Create Factorial.c
    Creación incial

---
+++ Factorial.c
@@ -0,0 +1,25 @@
+#Include <stdio.h>
+
+int main()
+{
+  int valor = 0
+  
+  int valor;
+  printf ("\n Inserte un valor del 0 al 5 \n);
+  scanf ("%d", &valor);
+  
+  long factorial (int valor);
+    //int valor = 5;
+    //int valor = 4;
+    //int valor = 3;
+    //int valor = 2;
+    //int valor = 1;
+    //int valor = 0;
+    if (n == 0) /* Caso base */
+      //return 1;
+      return 1; /* como 0! = 1, regresa 1*/
+    else
+      //return 3 * factorial (2);
+      //return 2 * factorial (1);
+      //return 1 * factorial (0)
+      return n * factorial (n - 1); /* llamada a esta misma funcion */
