a66827644cc57cb826891f08e3e88ab9ff124fdd
Abigail G <79664752+Abynana@users.noreply.github.com>
2021-06-07 12:22:04 -0500

    Update Factorial.c
    Se agregaron if y else al programa.

--- Factorial.c
+++ Factorial.c
@@ -15,11 +15,34 @@ int main()
     //int valor = 2;
     //int valor = 1;
     //int valor = 0;
-    if (n == 0) /* Caso base */
+    if (valor == 0) /* Caso base */
       //return 1;
       return 1; /* como 0! = 1, regresa 1*/
     else
+      //return 5 * factorial (4)
+      //return 4 * factorial (3)
       //return 3 * factorial (2);
       //return 2 * factorial (1);
       //return 1 * factorial (0)
-      return n * factorial (n - 1); /* llamada a esta misma funcion */
+      return valor * factorial (valor - 1); /* llamada a esta misma funcion */
+    if (valor == 1) 
+          //return 2;
+          return 2; 
+    else 
+      //return 5 * factorial (4)
+      //return 4 * factorial (3)
+      //return 3 * factorial (2);
+      //return 2 * factorial (1);
+          return valor * factorial (valor - 2);
+     if (valor == 2) 
+            //return 3;
+            return 3;
+     else 
+      //return 5 * factorial (4)
+      //return 4 * factorial (3)
+      //return 3 * factorial (2);
+          return valor * factorial (valor -3);
+     if (valor == 4)
+          }
+          
+          }
