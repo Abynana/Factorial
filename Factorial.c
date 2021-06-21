7088e0cb4233c3d00e5d8d61958c82513a1c42a0
Abigail G <79664752+Abynana@users.noreply.github.com>
2021-06-20 23:44:41 -0500

    Update Factorial.c
    Se cambio todo el código debido a que estaba mal hecho y no cumplía con los requisitos necesarios.

--- Factorial.c
+++ Factorial.c
@@ -1,48 +1,84 @@
-#Include <stdio.h>
+/*
+	- Como primer paso pediremos al usuario el valor que desea realizar el cálculo de factorial.
+	- Cambiar el tipo de dato para poder hacer cálculo más grande.
+	- Tendremos que validar la entrada del usuario mediante un número positivo, si este requisito no se cumple será invalidado.
+	- Cuando el cálculo de del usuario finalice, el programa deberá preguntar nuevamente si se desea hacer otro cálculo.
+	
+*/
 
-int main()
-{
-  int valor = 0
-  
-  int valor;
-  printf ("\n Inserte un valor del 0 al 5 \n);
-  scanf ("%d", &valor);
-  
-  long factorial (int valor);
-    //int valor = 5;
-    //int valor = 4;
-    //int valor = 3;
-    //int valor = 2;
-    //int valor = 1;
-    //int valor = 0;
-    if (valor == 0) /* Caso base */
-      //return 1;
-      return 1; /* como 0! = 1, regresa 1*/
-    else
-      //return 5 * factorial (4)
-      //return 4 * factorial (3)
-      //return 3 * factorial (2);
-      //return 2 * factorial (1);
-      //return 1 * factorial (0)
-      return valor * factorial (valor - 1); /* llamada a esta misma funcion */
-    if (valor == 1) 
-          //return 2;
-          return 2; 
-    else 
-      //return 5 * factorial (4)
-      //return 4 * factorial (3)
-      //return 3 * factorial (2);
-      //return 2 * factorial (1);
-          return valor * factorial (valor - 2);
-     if (valor == 2) 
-            //return 3;
-            return 3;
-     else 
-      //return 5 * factorial (4)
-      //return 4 * factorial (3)
-      //return 3 * factorial (2);
-          return valor * factorial (valor -3);
-     if (valor == 4)
-          }
-          
-          }
+#include <stdio.h>
+
+double factorial(double x);
+
+int main(){
+	
+	double numero, calculo;
+	int condicion;
+	
+	do{
+	
+		//Ingresamos el número, para obtener el factorial.
+		
+		printf("\n\nIngrese un numero: ");
+		scanf("%lf", &numero);
+		
+		
+		//Si el número es mayor a 0, se hará el cálculo del factorial. 
+		if(numero > 0){
+		
+			
+			calculo = factorial (numero);
+			
+			printf("El factorial del numero %.0lf es de: %.0lf ", numero, calculo);
+			
+			
+		}
+		
+		//En caso de que sea negativo, va a mostrar el siguiente mensaje, 
+		//hasta que el usuario ingrese un número positivo.
+		else{
+			
+			do{
+					
+				printf("\nOpcion invalida, intente de nuevo\n");
+				
+				printf("\nIngrese un numero positivo: ");
+				scanf("%lf", &numero);
+			
+				
+			}while(numero < 0);	
+			
+			calculo = factorial (numero);
+			
+			printf("\nEl factorial del numero %.0lf es de: %.0lf ", numero, calculo);
+						
+		}
+		
+			//El número 1 servirá para poder hacer otro cálculo.
+			//En caso contrario si oprime el 0 el programa llegará a su fin.
+			
+			printf("\n¿Desea realizar otro calculo? [Si=1][No=0]: ");
+			scanf("%d", &condicion);
+		
+	}while(condicion != 0); 
+		
+	//Una vez que el programa se terminé veremos la siguiente pantalla.
+		
+	printf("\nQue tenga un excelente día\n");
+	
+	return 0;
+}
+
+double factorial(double x){
+	
+	if(x == 0 || x == 1){
+		
+		return 1;
+	}
+	
+	else{
+		
+		return x * factorial (x - 1);
+	}
+	
+}
